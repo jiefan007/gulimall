42c7fa7e3fb1c7dfd38337dd10733ba70d693f05
jiefan007 <605522627@qq.com>
2020-12-20 15:42:22 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/atbaozun/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atbaozun.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
